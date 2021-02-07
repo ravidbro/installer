8508d6dec68466e2b79eb2a3b77ca18e44043142
Ravid Brown <ravid@redhat.com>
2021-02-07 16:29:21 +0200

    Approve CSR with kubectl
    Signed-off-by: Ravid Brown <ravid@redhat.com>

--- data/data/aio/files/usr/local/bin/approve-csr.sh
+++ data/data/aio/files/usr/local/bin/approve-csr.sh
@@ -5,8 +5,8 @@ KUBECONFIG="${1}"
 echo "Approving all CSR requests until bootstrapping is complete..."
 while [ ! -f /opt/openshift/.bootkube.done ]
 do
-    oc --kubeconfig="$KUBECONFIG" get csr --no-headers | grep Pending | \
+    kubectl --kubeconfig="$KUBECONFIG" get csr --no-headers | grep Pending | \
         awk '{print $1}' | \
-        xargs --no-run-if-empty oc --kubeconfig="$KUBECONFIG" adm certificate approve
+        xargs --no-run-if-empty kubectl --kubeconfig="$KUBECONFIG" certificate approve
 	sleep 20
 done
